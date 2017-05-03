097f47607210e27bcb6b4f4089bfaf00a75fc0b3
Tom K <tom.klopfstein@gmail.com>
2017-05-03 15:28:54 -0700

    Initial commit with LinqLambdaSamples class added but not edited.  Also added Northwind.sql script that can be used to generate the database. Also added Setting for connection string the defaults to using localdb.

---
+++ Csharp/SettingsForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace QuerySamples
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            this.CancelButton2 = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ConnectionTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CancelButton2
+            // 
+            this.CancelButton2.Location = new System.Drawing.Point(765, 70);
+            this.CancelButton2.Name = "CancelButton2";
+            this.CancelButton2.Size = new System.Drawing.Size(85, 37);
+            this.CancelButton2.TabIndex = 0;
+            this.CancelButton2.Text = "Cancel";
+            this.CancelButton2.UseVisualStyleBackColor = true;
+            this.CancelButton2.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(674, 70);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(85, 37);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // ConnectionTextBox
+            // 
+            this.ConnectionTextBox.Location = new System.Drawing.Point(12, 27);
+            this.ConnectionTextBox.Multiline = true;
+            this.ConnectionTextBox.Name = "ConnectionTextBox";
+            this.ConnectionTextBox.Size = new System.Drawing.Size(838, 37);
+            this.ConnectionTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Northwind Database Connection";
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 117);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ConnectionTextBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.CancelButton2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SettingsForm";
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.SettingsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CancelButton2;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox ConnectionTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Csharp/SettingsForm.cs
@@ -0,0 +1,39 @@
+﻿using SampleQueries;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuerySamples
+{
+    public partial class SettingsForm : Form
+    {
+        public SettingsForm()
+        {
+            InitializeComponent();
+
+        }
+        
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.NorthwindConnection = this.ConnectionTextBox.Text;
+            LinqToSqlSamples.ConnString = QuerySamples.Properties.Settings.Default.NorthwindConnection;
+
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SettingsForm_Load(object sender, EventArgs e)
+        {
+            this.ConnectionTextBox.Text = Properties.Settings.Default.NorthwindConnection;
+        }
+    }
+}
